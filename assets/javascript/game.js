8b3870a6b53dfb95b953703c8403b8c800377025
musicis3965 <bobby.timmons@rocketmail.com>
2018-09-08 09:45:54 -0400

    another one

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,19 @@
-<!doctype html>
 
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>title goes here</title>
-  <!-- <link rel="stylesheet" href="styles.css">  -->
-  <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
-  <!-- <script src="https://code.jquery.com/jquery.js"></script> -->
-</head>
 
-<body>
+// instructions that dissapear once the games starts
+
+// randomly generated number computer comes up with, that player has to get exactly to 'win'
+
+// wins and losses counter that goes up
+
+// four pictures, whose value change after every 'round' of the game
+
+// Here we created an on-click event that responds to button clicks of the krystal image.
+$(".images").on("click", function() {
   
+    // Clicking the button triggers an alert message.
+    alert("You clicked a crystal!");
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>  
-</body>
-</html>
+  });
+
+// total number player has, in order to 'win' or 'lose'
